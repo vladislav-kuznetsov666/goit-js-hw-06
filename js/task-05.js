0769bee69603f49bdcbe2918af36b64aa160e107
vladislav-kuznetsov666 <Kuznetsovv887@gmail.com>
2023-10-23 18:14:14 +0300

    3-th commit

--- js/task-05.js
+++
@@ -1,6 +0,0 @@
-const nameInput = document.getElementById("name-input");
-const nameOutput = document.getElementById("name-output");
-
-nameInput.addEventListener("input", () => {
-  nameOutput.textContent = nameInput.value.trim() || "Anonymous";
-});
